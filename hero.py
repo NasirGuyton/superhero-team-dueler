39a6b5a8ba3738b2f03cc88b63bcd12ee16125a9
Nasir Joseph Guyton-Barreau <nasirjoseph.guyton@students.dominican.edu>
2026-03-09 20:00:29 -0700

    implemented team attack and defense

--- hero.py
+++ hero.py
@@ -12,12 +12,16 @@ class Hero:
             name: String
             starting_health: Integer
             current_health: Integer
+            deaths: Integer
+            kills: Integer
         """
         self.abilities = []
         self.armors = []
         self.name = name
         self.starting_health = starting_health
         self.current_health = starting_health
+        self.deaths = 0
+        self.kills = 0
 
     def add_ability(self, ability):
         """Add ability to abilities list."""
@@ -26,6 +30,10 @@ class Hero:
     def add_weapon(self, weapon):
         """Add weapon to self.abilities."""
         self.abilities.append(weapon)
+
+    def add_armor(self, armor):
+        """Add armor to self.armors."""
+        self.armors.append(armor)
 
     def attack(self):
         """
@@ -36,12 +44,6 @@ class Hero:
         for ability in self.abilities:
             total_damage += ability.attack()
         return total_damage
-
-    def add_armor(self, armor):
-        """
-        Add armor to self.armors.
-        """
-        self.armors.append(armor)
 
     def defend(self):
         """
@@ -60,8 +62,7 @@ class Hero:
         """
         Updates self.current_health to reflect the damage minus the defense.
         """
-        defense = self.defend()
-        damage_taken = damage - defense
+        damage_taken = damage - self.defend()
 
         if damage_taken < 0:
             damage_taken = 0
@@ -73,6 +74,14 @@ class Hero:
         Return True if hero is alive, otherwise False.
         """
         return self.current_health > 0
+
+    def add_kill(self, num_kills):
+        """Update self.kills by num_kills amount."""
+        self.kills += num_kills
+
+    def add_death(self, num_deaths):
+        """Update deaths with num_deaths."""
+        self.deaths += num_deaths
 
     def fight(self, opponent):
         """
@@ -89,19 +98,33 @@ class Hero:
             opponent.take_damage(self_damage)
             self.take_damage(opponent_damage)
 
-            if self.is_alive() and not opponent.is_alive():
-                print(f"{self.name} won!")
-                return
-            elif opponent.is_alive() and not self.is_alive():
-                print(f"{opponent.name} won!")
-                return
-            elif not self.is_alive() and not opponent.is_alive():
-                print("Draw")
-                return
+        if self.is_alive() and not opponent.is_alive():
+            self.add_kill(1)
+            opponent.add_death(1)
+            print(f"{self.name} won!")
+        elif opponent.is_alive() and not self.is_alive():
+            opponent.add_kill(1)
+            self.add_death(1)
+            print(f"{opponent.name} won!")
+        elif not self.is_alive() and not opponent.is_alive():
+            self.add_death(1)
+            opponent.add_death(1)
+            print("Draw")
 
 
 if __name__ == "__main__":
-    hero = Hero("Wonder Woman")
-    weapon = Weapon("Lasso of Truth", 90)
-    hero.add_weapon(weapon)
-    print(hero.attack())
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+
+    hero1.add_ability(Ability("Super Strength", 200))
+    hero1.add_weapon(Weapon("Lasso of Truth", 100))
+    hero1.add_armor(Armor("Amazon Shield", 50))
+
+    hero2.add_ability(Ability("Magic Blast", 150))
+    hero2.add_weapon(Weapon("Wizard Staff", 80))
+    hero2.add_armor(Armor("Magic Barrier", 40))
+
+    hero1.fight(hero2)
+
+    print(hero1.name, "Kills:", hero1.kills, "Deaths:", hero1.deaths)
+    print(hero2.name, "Kills:", hero2.kills, "Deaths:", hero2.deaths)
--- team.py
+++ team.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Team:
     def __init__(self, name):
         """Initialize your team with its team name and an empty list of heroes."""
@@ -25,29 +28,86 @@ class Team:
             return 0
 
     def view_all_heroes(self):
-        """Print out all heroes to the console."""
+        """Prints out all heroes to the console."""
         for hero in self.heroes:
             print(hero.name)
+
+    def stats(self):
+        """Print team statistics."""
+        for hero in self.heroes:
+            if hero.deaths == 0:
+                kd = hero.kills
+            else:
+                kd = hero.kills / hero.deaths
+            print(f"{hero.name} Kill/Deaths:{kd}")
+
+    def revive_heroes(self, health=100):
+        """Reset all heroes health to starting_health."""
+        for hero in self.heroes:
+            hero.current_health = hero.starting_health
+
+    def attack(self, other_team):
+        """Battle each team against each other."""
+        living_heroes = []
+        living_opponents = []
+
+        for hero in self.heroes:
+            if hero.is_alive():
+                living_heroes.append(hero)
+
+        for hero in other_team.heroes:
+            if hero.is_alive():
+                living_opponents.append(hero)
+
+        while len(living_heroes) > 0 and len(living_opponents) > 0:
+            hero = random.choice(living_heroes)
+            opponent = random.choice(living_opponents)
+
+            hero.fight(opponent)
+
+            living_heroes = [hero for hero in self.heroes if hero.is_alive()]
+            living_opponents = [hero for hero in other_team.heroes if hero.is_alive()]
 
 
 if __name__ == "__main__":
     from hero import Hero
+    from ability import Ability
+    from armor import Armor
+    from weapon import Weapon
 
-    team = Team("Justice League")
+    team_one = Team("Justice League")
+    team_two = Team("Wizards")
 
     hero1 = Hero("Wonder Woman")
+    hero1.add_ability(Ability("Super Strength", 200))
+    hero1.add_weapon(Weapon("Lasso of Truth", 100))
+    hero1.add_armor(Armor("Bracelets", 40))
+
     hero2 = Hero("Batman")
-    hero3 = Hero("Superman")
+    hero2.add_ability(Ability("Martial Arts", 120))
+    hero2.add_weapon(Weapon("Batarang", 80))
+    hero2.add_armor(Armor("Suit Armor", 30))
 
-    team.add_hero(hero1)
-    team.add_hero(hero2)
-    team.add_hero(hero3)
+    hero3 = Hero("Dumbledore")
+    hero3.add_ability(Ability("Magic Blast", 180))
+    hero3.add_weapon(Weapon("Elder Wand", 110))
+    hero3.add_armor(Armor("Magic Shield", 35))
 
-    print("All heroes:")
-    team.view_all_heroes()
+    hero4 = Hero("Gandalf")
+    hero4.add_ability(Ability("Fire Spell", 160))
+    hero4.add_weapon(Weapon("Staff Strike", 90))
+    hero4.add_armor(Armor("Wizard Cloak", 25))
 
-    print("\nRemoving Batman...")
-    team.remove_hero("Batman")
+    team_one.add_hero(hero1)
+    team_one.add_hero(hero2)
 
-    print("\nAll heroes after removal:")
-    team.view_all_heroes()
+    team_two.add_hero(hero3)
+    team_two.add_hero(hero4)
+
+    team_one.attack(team_two)
+
+    print("\nTeam One Stats:")
+    team_one.stats()
+
+    print("\nTeam Two Stats:")
+    team_two.stats()
